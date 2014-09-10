27427be560d81f8ec05d157106675637cfafbf76
Shane StClair <shane@axiomalaska.com>
2014-09-10 01:19:08 -0700

    More update

--- generate_covered_tests.sh
+++ generate_covered_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-grep -hoPR '(?<=<ctl:test name=").*\:IOOS-SOS.*(?=")' $DIR/m1.0 | uniq | sort > $DIR/covered_tests.txt
+grep -hoIPR '(?<=<ctl:test name=").*\:IOOS-SOS.*(?=")' $DIR/m1.0 | uniq | sort > $DIR/covered_tests.txt
