af5ddaf2dcc885e74c7341879965416d8a71a54d
Shane StClair <shane@axiomalaska.com>
2014-12-12 13:49:32 -0800

    Include teamengine binary in release

--- dev/build_release.sh
+++ dev/build_release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 command -v git >/dev/null 2>&1 || { echo "git command must be installed" >&2; exit 1; }
+command -v zip >/dev/null 2>&1 || { echo "zip command must be installed" >&2; exit 1; }
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$DIR"/..
 if [[ $# -ne 1 ]]; then
@@ -7,8 +8,16 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 TAG=$1
-
+OUT=ioos-sos-compliance-tests-$TAG
 cd $ROOT
 rm -rf target
 mkdir target
-git archive -o target/ioos-sos-compliance-tests-$TAG.zip --prefix=ioos-sos-compliance-tests-$TAG/ $TAG
+git archive -o target/$OUT.zip --prefix=$OUT/ $TAG
+
+rm -rf tmp
+mkdir -p tmp/$OUT
+cd tmp
+cp -r $ROOT/teamengine $OUT
+zip -rq $ROOT/target/$OUT.zip $OUT
+cd $ROOT
+rm -rf tmp
