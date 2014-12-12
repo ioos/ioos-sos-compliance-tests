435767b3c3074a449be7835c9d1f2e7462f41323
Shane StClair <shane@axiomalaska.com>
2014-12-12 13:29:11 -0800

    Change releases to use git tags

--- dev/build_release.sh
+++ dev/build_release.sh
@@ -3,13 +3,12 @@ command -v git >/dev/null 2>&1 || { echo "git command must be installed" >&2; ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$DIR"/..
 if [[ $# -ne 1 ]]; then
-  echo "Provide release version as argument"
+  echo "Provide release tag as argument"
   exit 1
 fi
-VERSION=$1
+TAG=$1
 
-"$DIR"/generate_covered_tests.sh
 cd $ROOT
 rm -rf target
 mkdir target
-git archive -o target/ioos-sos-compliance-tests-$VERSION.zip HEAD
+git archive -o target/ioos-sos-compliance-tests-$TAG.zip --prefix=ioos-sos-compliance-tests-$TAG/ $TAG
