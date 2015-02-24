3b3e0d68ccced2fcc583f1dff2989c612360a46b
Shane StClair <shane@axiomdatascience.com>
2015-02-23 22:31:45 -0800

    Add windows support (run_tests.bat)

---
+++ run_tests.bat
@@ -0,0 +1,28 @@
+@echo off
+
+SET DIR=%~dp0
+SET CONFIGFILE=%~f1
+SET TESTS=%2
+SET CTLDIR=%DIR%m1.0\ctl
+
+IF [%CONFIGFILE%] == [] (
+  ECHO Must provide config file path as argument 1>&2
+  EXIT /B 1
+)
+
+IF NOT EXIST "%CONFIGFILE%" (
+  ECHO Config file %CONFIGFILE% doesn't exist 1>&2
+  EXIT /B 1
+)
+
+IF NOT EXIST "%CTLDIR%" (
+  ECHO CTL directory %CTLDIR% not found 1>&2
+  EXIT /B 1
+)
+
+IF NOT [%TEST%] == [] (
+  SET TESTSPARAM=@tests="%TESTS%"
+)
+
+SET CONFIGPARAM=%CONFIGFILE:\=/%
+%DIR%/teamengine/bin/windows/test.bat -source="%CTLDIR%" @configFile="%CONFIGPARAM%" @rootDir="%DIR%" %TESTSPARAM%
